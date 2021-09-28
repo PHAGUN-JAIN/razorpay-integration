238e3fc49175b5225bc0bbfd3521263742d972bb
PHAGUN-JAIN <phagunjain@protonmail.com>
2021-09-28 15:56:36 +0530

    fixed order id

--- server.js
+++ server.js
@@ -19,20 +19,22 @@ app.get("/payments", (req, res) => {
   res.end();
 });
 
-app.post("/payments/order", (req, res) => {
+app.post("/payments/order", async (req, res) => {
   console.log("create orderid request to razor pay", req.body);
 
   var options = {
     amount: 50000, // amount in the smallest currency unit
     currency: "INR",
     receipt: "order_rcptid_11",
+    payment_capture: 1,
   };
 
-  instance.orders.create(options, function (err, order) {
+  await instance.orders.create(options, (err, order) => {
     if (err) {
       console.log(err);
     } else {
       console.log(order);
+      console.log(order.id);
       res.send({ orderId: order.id });
     }
     // console.log(order);
@@ -40,27 +42,27 @@ app.post("/payments/order", (req, res) => {
   });
 });
 
-// app.post("/api/payment/verify", (req, res) => {
-//   let body =
-//     req.body.response.razorpay_order_id +
-//     "|" +
-//     req.body.response.razorpay_payment_id;
-//   var crypto = require("crypto");
-//   var expectedSignature = crypto
-//     .createHmac("sha256", process.env.KEY_SECRET)
-//     .update(body.toString())
-//     .digest("hex");
+app.post("/api/payment/verify", (req, res) => {
+  let body =
+    req.body.response.razorpay_order_id +
+    "|" +
+    req.body.response.razorpay_payment_id;
+  var crypto = require("crypto");
+  var expectedSignature = crypto
+    .createHmac("sha256", process.env.KEY_SECRET)
+    .update(body.toString())
+    .digest("hex");
 
-//   console.log("sig received ", req.body.response.razorpay_signature);
+  console.log("sig received ", req.body.response.razorpay_signature);
 
-//   console.log("sig generated ", expectedSignature);
+  console.log("sig generated ", expectedSignature);
 
-//   var response = { signatureIsValid: "false" };
+  var response = { signatureIsValid: "false" };
 
-//   if (expectedSignature === req.body.response.razorpay_signature)
-//     response = { signatureIsValid: "true" };
-//   res.send(response);
-// });
+  if (expectedSignature === req.body.response.razorpay_signature)
+    response = { signatureIsValid: "true" };
+  res.send(response);
+});
 
 app.listen(PORT, () => {
   console.log(`listining to port ${PORT}`);
